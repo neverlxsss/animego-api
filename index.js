ea6e2c7d4f1ea466c25164ce4e6d2895df1f1074
Tony Sanin <sanin.tony.dev@gmail.com>
2024-01-07 02:57:15 +0200

    moved index, added tests

--- index.js
+++
@@ -1,8 +0,0 @@
-const { search, info, streamInfo, stream } = require('./src/Animego');
-
-module.exports = {
-    search,
-    info,
-    streamInfo,
-    stream,
-};
---
+++ tests/animego.js
@@ -0,0 +1,39 @@
+import {describe, it, expect} from 'vitest';
+import {info, search, stream, streamInfo} from '../src/';
+
+describe('testing Animego', () => {
+    let url = null;
+
+    it('Search correctly', async () => {
+        const searchResults = await search("tokyo ghoul");
+
+        expect(searchResults.success).toBe(true);
+        searchResults.data.forEach(result => {
+            expect(result.url).contains("https");
+            expect(result.image).toBeDefined();
+            expect(result.rate).toBeDefined();
+            expect(result.originalTitle).toBeDefined();
+            expect(result.title).toBeDefined();
+            expect(result.year).toBeDefined();
+        });
+    });
+
+    it('Gets info correctly', async () => {
+        const infoResult = await info("https://animego.org/anime/dobro-pozhalovat-v-klass-prevoshodstva-3-2465");
+
+        expect(infoResult.success).toBe(true);
+    });
+
+    it('Gets stream info correctly', async () => {
+        const streamInfoResult = await streamInfo("https://animego.org/anime/dobro-pozhalovat-v-klass-prevoshodstva-3-2465");
+
+        expect(streamInfoResult.success).toBe(true);
+    });
+
+    it('Gets stream correctly', async () => {
+        const streamInfoResult = await streamInfo("https://animego.org/anime/dobro-pozhalovat-v-klass-prevoshodstva-3-2465");
+        const streamResult = await stream("https://animego.org/anime/dobro-pozhalovat-v-klass-prevoshodstva-3-2465", 1, streamInfoResult.data.players[0], streamInfoResult.data.translations[0]);
+
+        expect(streamResult.success).toBe(true);
+    });
+});
